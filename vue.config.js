bff0960ed2233623b525da67573b9591b30dd8a4
mzy <503391635@qq.com>
2019-12-23 13:36:10 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+let path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+  }
+}
